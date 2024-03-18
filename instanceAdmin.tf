03b35794702c200e94274c45061aaf52781477d7
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-03-18 19:37:56 -0400

    Create instanceAdmin.tf

---
+++ instanceAdmin.tf
@@ -0,0 +1,18 @@
+# Get list of student emails 
+variable "student_emails" {
+  type = list(string)
+  default = [
+    "jaimeh26@gmail.com",
+    "husni.khabbaz@gmail.com",
+    "student3@example.com"
+  ]
+}
+
+# Grant IAM permission to each student email
+resource "google_project_iam_member" "instance_admin" {
+  count = length(var.student_emails)
+
+  project = "my-project"
+  role    = "roles/compute.instanceAdmin"
+  member  = "user:${var.student_emails[count.index]}"
+}
